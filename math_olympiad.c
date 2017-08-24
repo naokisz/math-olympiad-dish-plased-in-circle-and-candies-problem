9e475ed1bc8fa7e8e6180a5086dfe9a219c6ddd7
naoki suzuki <naokihirokitomoki@gmail.com>
2017-08-25 08:52:52 +0900

    皿の数を変えられるようにした

--- math_olympiad.c
+++ math_olympiad.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 int int_sort(const void *a,const void *b) {
 	if(*(int*)a < *(int*)b) {
@@ -12,41 +13,79 @@ int int_sort(const void *a,const void *b) {
 	return 1;
 }
 
+int int_pointer_sort(const void *a, const void *b) {
+	if(**(int**)a < **(int**)b) {
+		return -1;
+	}
+	else if(**(int**)a == **(int**)b) {
+		return 0;
+	}
+	return 1;
+}
+int max_sum_drops(int x) {
+
+	return pow(x, 2) - x + 1;
+}
+
 int main() {
 
-	int num_of_drops[5];
-	int sum_drops[21];
-	int space_of_two;
+	int num_of_dish;
+	int *dishes;
+	int **tmp_dishes;
+	int *sum_drops;
 	int i, j, k, times;
+	int head, add_times, add_count;
 
 	srand(time(NULL));
+	printf("皿の数は？>");
+	scanf("%d", &num_of_dish);
+	dishes = (int*)malloc(num_of_dish * sizeof(int));
+	sum_drops = (int*)malloc(max_sum_drops(num_of_dish) * sizeof(int));
+	tmp_dishes = (int**)malloc(num_of_dish * sizeof(int*));
+
 	for(times = 1;;times++) {
+		for(;;) {
+			for(i = 0;i < num_of_dish;i++) {
+				dishes[i] = 0;
+			}
+	
+			dishes[0] = 1;
+			for(i = 0;i < (max_sum_drops(num_of_dish) - 1);i++) {
+				dishes[(int)(((double)rand() / RAND_MAX) * (num_of_dish - 1) + 1)]++;
+			}
+			for(i = 0;i < num_of_dish;i++) {
+				tmp_dishes[i] = &dishes[i];
+			}
+	
+			qsort(tmp_dishes, num_of_dish, sizeof(int*), int_pointer_sort);
 
-		num_of_drops[0] = 1;
-		space_of_two = (int)(((double)rand() / RAND_MAX) * 2) + 2;
-		num_of_drops[space_of_two] = 2;
-
-		num_of_drops[1] = (int)(((double)rand() / RAND_MAX) * 16) + 1;
-		num_of_drops[4] = (int)(((double)rand() / RAND_MAX) * (17 - num_of_drops[1])) + 1;
-		if(space_of_two == 2) {
-			num_of_drops[3] = 18 - num_of_drops[1] - num_of_drops[4];
-		} else {
-			num_of_drops[2] = 18 - num_of_drops[1] - num_of_drops[4];
+			for(i = 0;i < (num_of_dish - 1);i++) {
+				if(*tmp_dishes[i] == *tmp_dishes[i + 1]) {
+					break;
+				} else if(i == (num_of_dish - 2) && *tmp_dishes[i] != *tmp_dishes[i + 1]) {
+					goto success_rand;
+				}
+			}
 		}
 
-		for(i = 0;i < 21;i++) {
+		success_rand:
+
+		for(i = 0;i < max_sum_drops(num_of_dish);i++) {
 			sum_drops[i] = 0;
 		}
 
+/*
 		printf("num_of_drops:");
-		for(i = 0;i < 5;i++) {
-			printf("%d ", num_of_drops[i]);
+		for(i = 0;i < num_of_dish;i++) {
+			printf("%d ", dishes[i]);
 		}
 		printf("\n");
+*/
 
-		for(i = 0;i < 4;i++) {
-			for(j = 0;j < 5;j++) {
-				for(k = 0;k <= i;k++) {
+/*
+		for(i = 0;i < 4;i++) { //足す回数
+			for(j = 0;j < 5;j++) { //dishのカウンタ
+				for(k = 0;k <= i;k++) { //足すのは何回目か
 					sum_drops[i * 5 + j] += num_of_drops[(j + k) % 5];
 				}
 			}
@@ -54,13 +93,32 @@ int main() {
 		for(i = 0;i < 5;i++) {
 			sum_drops[20] += num_of_drops[i];
 		}
+*/
+		for(head = 0;head < num_of_dish;head++) {
+			for(add_times = 0;add_times < (num_of_dish - 1);add_times++) {
+				for(add_count = 0;add_count <= add_times;add_count++) {
+					sum_drops[head * (num_of_dish - 1) + add_times] += dishes[(head + add_count) % num_of_dish];
+				}
+			}
+		}
+		for(add_count = 0;add_count < num_of_dish;add_count++) {
+			sum_drops[max_sum_drops(num_of_dish) - 1] += dishes[add_count];
+		}
 
-		qsort(sum_drops, 21, sizeof(int), int_sort);
+		qsort(sum_drops, max_sum_drops(num_of_dish), sizeof(int), int_sort);
 
-		for(i = 0;i < 21;i++) {
+/*
+		printf("sum:");
+		for(i = 0;i < max_sum_drops(num_of_dish);i++) {
+			printf("%d ", sum_drops[i]);
+		}
+		printf("\n");
+*/
+
+		for(i = 0;i < max_sum_drops(num_of_dish);i++) {
 			if(sum_drops[i] != i + 1) {
 				break;
-			} else if(i == 20) {
+			} else if(i == (max_sum_drops(num_of_dish) -1)) {
 				goto success;
 			}
 		}
@@ -68,8 +126,8 @@ int main() {
 
 	success:
 	printf("answer:");
-	for(i = 0;i < 5;i++) {
-		printf("%d ", num_of_drops[i]);
+	for(i = 0;i < num_of_dish;i++) {
+		printf("%d ", dishes[i]);
 	}
 	printf("\n");
 	printf("times:%d\n", times);
